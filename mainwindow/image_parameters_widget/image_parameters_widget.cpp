3a028601dbec6f844e6c28b5d45a3a77d8b818ee
Alexandr Novgorodov <53829252+shamanking481516@users.noreply.github.com>
2022-02-06 19:57:55 +0700

    [*] Изменяемые параметры виджета в члены класса

--- mainwindow/image_parameters_widget/image_parameters_widget.cpp
+++ mainwindow/image_parameters_widget/image_parameters_widget.cpp
@@ -15,22 +15,31 @@ ImageParametersWidget::~ImageParametersWidget()
 
 }
 
+void ImageParametersWidget::setMat(const cv::Mat &t_mat)
+{
+
+}
+
 void ImageParametersWidget::setupUi()
 {
     auto *layout{new QVBoxLayout};
     auto *form_layout{new QFormLayout};
     auto *mean_label = new QLabel(tr("Mean:"));
-    auto *mean_line_edit = new QLineEdit;
-    form_layout->addRow(mean_label, mean_line_edit);
+    m_mean_line_edit = new QLineEdit;
+    m_mean_line_edit->setReadOnly(true);
+    form_layout->addRow(mean_label, m_mean_line_edit);
     auto *median_label = new QLabel(tr("Median:"));
-    auto *median_line_edit = new QLineEdit;
-    form_layout->addRow(median_label, median_line_edit);
+    m_median_line_edit = new QLineEdit;
+    m_median_line_edit->setReadOnly(true);
+    form_layout->addRow(median_label, m_median_line_edit);
     auto *min_label = new QLabel(tr("Minimum:"));
-    auto *min_line_edit = new QLineEdit;
-    form_layout->addRow(min_label, min_line_edit);
+    m_min_line_edit = new QLineEdit;
+    m_min_line_edit->setReadOnly(true);
+    form_layout->addRow(min_label, m_min_line_edit);
     auto *max_label = new QLabel(tr("Maximum:"));
-    auto *max_line_edit = new QLineEdit;
-    form_layout->addRow(max_label, max_line_edit);
+    m_max_line_edit = new QLineEdit;
+    m_max_line_edit->setReadOnly(true);
+    form_layout->addRow(max_label, m_max_line_edit);
     layout->addLayout(form_layout);
     layout->addStretch(10);
     setLayout(layout);
--- mainwindow/image_parameters_widget/image_parameters_widget.hpp
+++ mainwindow/image_parameters_widget/image_parameters_widget.hpp
@@ -2,6 +2,9 @@
 #define IMAGE_PARAMETERS_WIDGET_HPP
 
 #include <QWidget>
+#include <opencv2/opencv.hpp>
+
+class QLineEdit;
 
 class ImageParametersWidget : public QWidget
 {
@@ -10,7 +13,15 @@ public:
     explicit ImageParametersWidget(QWidget *t_parent = nullptr);
     virtual ~ImageParametersWidget();
 
+public slots:
+    void setMat(const cv::Mat &t_mat);
+
 private:
+    QLineEdit *m_mean_line_edit;
+    QLineEdit *m_median_line_edit;
+    QLineEdit *m_min_line_edit;
+    QLineEdit *m_max_line_edit;
+
     void setupUi();
 };
 
